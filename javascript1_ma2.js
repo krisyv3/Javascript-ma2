82b5bb54423468b2eb99dfca7fa16504da9b0304
kriss <krisyv3@hotmail.com>
2019-09-17 18:04:25 +0200

    Task 4 is done

--- javascript1_ma2.js
+++ javascript1_ma2.js
@@ -16,7 +16,18 @@ console.log(myarray);
 
 
 //4. Given the following paragraph, create a JavaScript function that changes all mentions of strawberry to banana and strawberries to bananas: 
-Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.
+
+var mystr = "Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.";
+var myObject = {
+    Strawberries:"Banana's",
+    strawberries:"banana's",
+    strawberry:"banana",
+    Strawberry:"Banana"
+};
+str = mystr.replace(/Strawberries|strawberries|strawberry|Strawberry/gi, function(matched){
+    return myObject[matched];
+})
+console.log(str);
 
 
 //5. Create an array and a button. The array should contain 4 football clubs names. When the button is clicked, remove all elements out of the array that we just created. There after add in 4 names of cars inside that array. Log it to the console.
